d97540be12a348a4533addb1a20c61a4aaddbf40
ahmad aiman <ahmadaimanalias@gmail.com>
2026-06-02 11:15:17 +0800

    fix: change back to lax

--- src/auth_handler.rs
+++ src/auth_handler.rs
@@ -339,7 +339,7 @@ async fn mint_new_credentials(
     let cookie = Cookie::build(("refresh_token", cookie_value))
         .http_only(true)
         .secure(true)
-        .same_site(SameSite::None)
+        .same_site(SameSite::Lax)
         .max_age(Duration::days(1))
         .path("/auth")
         .build();
